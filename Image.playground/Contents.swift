b5086bf45b69c63d102e8b8d239893f9cfc0bad2
Fawaz <Fawaz.mezher@ocsbstudent.ca>
2018-09-24 23:02:27 -0400

    K

---
+++ Image.playground/Contents.swift
@@ -0,0 +1,42 @@
+
+
+// Created on: sept 
+// Created by: fawaz
+// Created for: ICS3U
+// This program is the UIKit solution for displaying an image
+// this will be commented out when code moved to Xcode
+import PlaygroundSupport
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    // this is the main view controller, that will show the UIKit elements
+    
+    // properties
+    //let image = UIImage(named: "IMG_1612.JPG")
+    let imageView = UIImageView(image: UIImage(named: "IMG_0018.JPG"))
+    
+    override func viewDidLoad() {
+        // UI
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.view = view
+        
+        
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+// this will be commented out when code moved to Xcode
+PlaygroundPage.current.liveView = ViewController()
